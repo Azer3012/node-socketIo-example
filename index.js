3946209347541e4c8686138962da962a331e0aac
azer <azer@azers-MacBook-Pro.local>
2022-11-02 03:58:01 +0400

    room message disconnnect

--- index.js
+++ index.js
@@ -7,26 +7,56 @@ const httpServer=http.createServer()
 const io=new Server(httpServer)
 
 httpServer.on("request",(req,res)=>{
-    if(req.url==='/'){
+
+    if(req.url==='/s'){
         const fileContent=fs.readFileSync('index.html')
-        res.end(fileContent.toString())
+         res.end(fileContent.toString())
+
     }
-   
+    
+    
+
 })
 
-io.on("connection",(socket)=>{
-    console.log("client connected");
-    socket.on("new user",(username)=>{
-        //tutalim chatda bir nece adam var bu broadcast ile menden basqa hamiya bu event gedir
-        socket.broadcast.emit("new user",username)
+const chatNamepaces=io.of("chat")
+
+const socketMap={}
+
+chatNamepaces.on("connection",(socket)=>{
+    console.log("client connected id: "+socket.id);
+
+
+    socket.on("join room",(data)=>{
+        socket.join(data.room)
+        socketMap[socket.id]=data
+        socket.to(data.room).emit("new user",data.username)
     })
+
+
+    
 
     
 
     socket.on("new message",(data)=>{
-        socket.broadcast.emit(data)
+        const {username,message}=data
+
+        const currentData=socketMap[socket.id]
+
+        socket.to(currentData.room).emit("new message",{username,message})
+    })
+
+    socket.on("disconnect",()=>{
+        console.log('client disconnected');
+
+        const currentData=socketMap[socket.id]
+
+        socket.to(currentData.room).emit("user left",currentData.username)
     })
 })
+
+
+
+
 
 
 
